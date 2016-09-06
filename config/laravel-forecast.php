13cccf352690b90990441123ec0e8b33bd6dad15
AdrianBav <adrian@bavanco.co.uk>
2016-09-05 21:33:20 -0500

    Completed world temperatues card config

---
+++ config/laravel-forecast.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Forecast.io Api key
+    |--------------------------------------------------------------------------
+    */
+    'API_KEY' => 'e98815f918636b04a373bb3ca21c8e49',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Adding additional options on the request to Forecast.io
+    |--------------------------------------------------------------------------
+    | For more details and all available options check the official documentation.
+    | https://developer.forecast.io/docs/v2
+    */
+    'options' => [
+        'units' => 'si',
+    ],
+];
