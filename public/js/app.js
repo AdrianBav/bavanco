368247d101f84149f7cfb08a446e10d91831d49a
Adrian Bavister <adrian@bavanco.co.uk>
2019-05-30 21:12:28 -0500

    Package update

--- public/js/app.js
+++ public/js/app.js
@@ -16114,8 +16114,8 @@ window.onscroll = function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /app/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /app/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/Bavanco/www/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/Bavanco/www/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
