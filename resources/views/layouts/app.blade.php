ec9ebd578164d35996bb4b66d3025a33a9ab5dda
Adrian Bavister <adrian@bavanco.co.uk>
2019-07-11 22:20:02 -0500

    Added color for mobile bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="theme-color" content="#263489">
 
         <!-- SEO Meta details -->
         <title>{{ config('app.name') }} | @yield('slogan', 'bee-a-vee-a-en-cee-o')</title>
