d3330fb8da573a34de9fa43c49cfcf10e80a2038
Adrian Bavister <adrian@bavanco.co.uk>
2019-11-11 22:50:09 -0600

    Updated lonopoly card

--- resources/views/decks/home/lonopolystreets.blade.php
+++ resources/views/decks/home/lonopolystreets.blade.php
@@ -17,15 +17,15 @@
 
 
 @section('details')
-    STREETS—Since @right($since['year'])<br>
-    @ditto('STREETS') number of years @right($since['years'])<br>
-    @ditto('STREETS') @ditto('number') @ditto('of') months @right($since['months'])<br>
-    @ditto('STREETS') @ditto('number') @ditto('of') weeks @right($since['weeks'])<br>
-    @ditto('STREETS') @ditto('number') @ditto('of') days @right($since['days'])<br>
-    @ditto('STREETS') TOTAL days @right($since['totalDays'])<br>
+    PROJECT—Since @right($since['year'])<br>
+    @ditto('PROJECT') number of years @right($since['years'])<br>
+    @ditto('PROJECT') @ditto('number') @ditto('of') months @right($since['months'])<br>
+    @ditto('PROJECT') @ditto('number') @ditto('of') weeks @right($since['weeks'])<br>
+    @ditto('PROJECT') @ditto('number') @ditto('of') days @right($since['days'])<br>
+    @ditto('PROJECT') TOTAL days @right($since['totalDays'])<br>
     <small>My personal collection of photographs taken at and around each of the streets on the London monopoly board.</small><br>
-    STREETS has {{ $meta->itemOne }}<br>
-    @ditto('STREETS') @ditto('has') {{ $meta->itemTwo }}<br>
-    @spacer('STREETS') @spacer('has') {{ $meta->info }}<br>
+    PROJECT has {{ $meta->itemOne }}<br>
+    @ditto('PROJECT') @ditto('has') {{ $meta->itemTwo }}<br>
+    @spacer('PROJECT') @spacer('has') {{ $meta->info }}<br>
     VISITS since launch, {{ $visits }}
 @overwrite
